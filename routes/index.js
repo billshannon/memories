e3c3a78d4f6620da713bb51df10fb5d8178f9159
Bill Shannon <bill@billshannon.com>
2015-11-13 10:03:43 -0700

    add route unique list of years'

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var pg = require('pg');
 
 var conString = process.env.DATABASE_URL || "postgres://@localhost/memoriesapp";
 
-router.post('/api/v1/memories', function(req, res, next) {
-    pg.connect(conString, function(err, client, done) {
+router.post('/api/v1/memories', function (req, res, next) {
+    pg.connect(conString, function (err, client, done) {
         if (err) {
             return console.error('error fetching client from pool', err);
         }
-        client.query('INSERT into memories(old_days, these_days, year) VALUES($1,$2,$3) ', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
+        client.query('INSERT into memories(old_days, these_days, year) VALUES($1,$2,$3) ', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function (err, result) {
             done();
             res.status(200).end();
             if (err) {
@@ -21,12 +21,12 @@ router.post('/api/v1/memories', function(req, res, next) {
     });
 });
 
-router.get('/api/v1/memories', function(req, res, next) {
-    pg.connect(conString, function(err, client, done) {
+router.get('/api/v1/memories', function (req, res, next) {
+    pg.connect(conString, function (err, client, done) {
         if (err) {
             return console.error('error fetching client from pool', err);
         }
-        client.query('SELECT * from memories', function(err, result) {
+        client.query('SELECT * from memories', function (err, result) {
             done();
             res.json(result.rows);
             if (err) {
@@ -37,12 +37,12 @@ router.get('/api/v1/memories', function(req, res, next) {
     });
 });
 
-router.get('/api/v1/memories/:year', function(req, res, next) {
-    pg.connect(conString, function(err, client, done) {
+router.get('/api/v1/memories/years', function (req, res, next) {
+    pg.connect(conString, function (err, client, done) {
         if (err) {
             return console.error('error fetching client from pool', err);
         }
-        client.query('SELECT * from memories WHERE year=($1)', [req.params.year], function(err, result) {
+        client.query('SELECT DISTINCT year from memories', function (err, result) {
             done();
             res.json(result.rows);
             if (err) {
@@ -52,5 +52,23 @@ router.get('/api/v1/memories/:year', function(req, res, next) {
         });
     });
 });
+
+
+router.get('/api/v1/memories/:year', function (req, res, next) {
+    pg.connect(conString, function (err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * from memories WHERE year=($1)', [req.params.year], function (err, result) {
+            done();
+            res.json(result.rows);
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log('connected to db')
+        });
+    });
+});
+
 
 module.exports = router;
