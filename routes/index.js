f75ff577c5596d37ef2b455d5aab5a1dfc667b1c
Bill Shannon <bill@billshannon.com>
2015-11-13 14:43:24 -0700

    fix return

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post('/api/v1/memories', function (req, res, next) {
         }
         client.query('INSERT into memories(old_days, these_days, year) VALUES($1,$2,$3) ', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function (err, result) {
             done();
-            res.status(200).end();
+            res.json(result)
             if (err) {
                 return console.error('error running query', err);
             }
@@ -53,7 +53,6 @@ router.get('/api/v1/memories/years', function (req, res, next) {
     });
 });
 
-
 router.get('/api/v1/memories/:year', function (req, res, next) {
     pg.connect(conString, function (err, client, done) {
         if (err) {
@@ -70,5 +69,20 @@ router.get('/api/v1/memories/:year', function (req, res, next) {
     });
 });
 
+router.post('/api/v1/memories/delete', function (req, res, next) {
+    pg.connect(conString, function (err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('DELETE from memories', function (err, result) {
+            done();
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.status(200).end()
+            console.log('records deleted')
+        });
+    });
+});
 
 module.exports = router;
